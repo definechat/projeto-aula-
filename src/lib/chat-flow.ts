4bc90ce0db577d301cae4257b81684e589eb1d34
Mike Saraiva <mikewille2@gmail.com>
2025-08-03 00:42:02 +0000

    apos a mensagem " Receba o seu gratuitamente! 🎁 Me diz teu nome. " adic

--- src/lib/chat-flow.ts
+++ src/lib/chat-flow.ts
@@ -22,6 +22,14 @@ export const chatFlow: ChatStep[] = [
     type: 'text',
     content: "Receba o seu **gratuitamente!** 🎁\nMe diz teu nome.",
     delay: 7000,
+  },
+  {
+    id: 0.35,
+    sender: 'bot',
+    type: 'audio',
+    audioSrc: 'https://serene-cuchufli-59aba5.netlify.app/audio/nome-whatsapp.mp3',
+    audioDuration: 7,
+    delay: 2000,
     waitForUser: true,
   },
   {
